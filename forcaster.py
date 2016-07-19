88d016176806fffa62a7fa525651a6a4f3b616cd
Robert Thompson <rthompsonj@gmail.com>
2016-07-19 00:37:12 -0500

    adding mongo connection and forcaster class.

---
+++ forcaster.py
@@ -0,0 +1,39 @@
+import urllib2
+import json
+from geopy.geocoders import Nominatim
+from mongo_connection import MongoConnection
+from api_key import APIKEY
+
+REINIT = False
+URL    = 'https://api.forecast.io/forecast'
+
+class ForcastRetriever(object):
+    
+    def __init__(self):
+        self.geolocator = Nominatim()
+        self.db = MongoConnection(reinit=REINIT)
+        
+    def _get_url(self, latitude, longitude):
+        return '%s/%s/%f,%f' % (URL, APIKEY, latitude, longitude)
+
+    def _get_location(self, loc_request):
+        try:
+            loc = self.geolocator.geocode(loc_request)
+            return loc
+        except:
+            raise ValueError('Enter a valid geocode!')
+        
+    def get_forcast(self, loc_request):
+        loc = self._get_location(loc_request)
+        
+        if self.db.update_location(loc.latitude, loc.longitude):
+            print('Refreshing location')
+            url = self._get_url(loc.latitude, loc.longitude)
+            request = urllib2.urlopen(url).read()
+            data = json.loads(request)
+            self.db.cache_location(data)
+        else:
+            print('Getting cached location')
+            data = self.db.get_location(loc.latitude, loc.longitude)
+            
+        return data
---
+++ mongo_connection.py
@@ -0,0 +1,75 @@
+import datetime
+from pymongo import MongoClient
+
+DBNAME = 'weather'
+
+class MongoConnection(object):
+    def __init__(self, ip='localhost', port=27015, reinit=False):
+        self.connection = MongoClient()
+
+        if DBNAME in self.connection.database_names() and not reinit:
+            self.db = self.connection[DBNAME]
+        else:
+            self.init_db()
+        
+    def init_db(self):
+        """Initialize the database and collections."""
+        self.connection.drop_database(DBNAME)
+        
+        # DB
+        self.db = self.connection[DBNAME]
+
+        # Collections
+        self.db.users.insert_one(
+            {
+                'username':'rthompson',
+                'password':'password'
+            }
+        )
+        
+        #self.db.cache.insert_one(
+        #    {
+        #        'zip':78665,
+        #        'name':'austin',
+        #        'state':'texas'
+        #    }
+        #)
+
+    def get_strftime(self):
+        """Converts current time into POSIX time integer."""
+        return int(datetime.datetime.now().strftime('%s'))
+
+    def get_location(self, latitude, longitude):
+        """Gets a location in the DB.  If not present returns None."""
+        return self.db.cache.find_one({
+            'latitude' :round(latitude, 6),
+            'longitude':round(longitude, 6),
+        })
+    
+    def update_location(self, latitude, longitude):
+        """Returns true/false depending on if we need to request
+        a new forcast.  True when the time last cached has exceeded
+        10 minutes or the location does not exist in the cache.  False
+        otherwise."""
+        current_time = self.get_strftime()
+        result = self.get_location(latitude, longitude)
+
+        if result is None:
+            return True
+        else:
+            if (current_time - result['time_updated']) > 10.*60.: # 10 minutes
+                return True
+            else:
+                return False
+            
+    def cache_location(self, data):
+        """Cache the given location to the db"""
+        result = self.get_location(data['latitude'], data['longitude'])
+        
+        data['time_updated'] = self.get_strftime()
+        if result is not None:
+            print('Updating entry')
+            self.db.cache.update({'_id':result['_id']}, data)
+        else:
+            print('Creating entry')
+            self.db.cache.insert_one(data)
