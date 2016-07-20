fecfc75a95b1fafb644697226dd3c52d443af321
Robert Thompson <rthompsonj@gmail.com>
2016-07-20 09:51:37 -0500

    moving the icon mapper to its own file.  small reorg of the flask app function ordering.

--- flask_app.py
+++ flask_app.py
@@ -1,25 +1,17 @@
 from flask import Flask, request, render_template, url_for, session, redirect
+from icon_mapper import icons
+from forecaster import ForecastRetriever
 
 app = Flask(__name__)
 
-from forecaster import ForecastRetriever
 FORECASTER = ForecastRetriever()
 
-icons = {
-    'clear-day': 'day-sunny',
-    'clear-night': 'night-clear',
-    'rain': 'rain',
-    'snow': 'snow',
-    'sleet': 'sleet',
-    'wind': 'strong-wind',
-    'fog': 'fog',
-    'cloudy': 'cloudy',
-    'partly-cloudy-day': 'day-cloudy',
-    'partly-cloudy-night': 'night-cloudy',
-    'hail': 'hail',
-    'thunderstorm': 'thunderstorm',
-    'tornado': 'tornado'
-}
+@app.route('/')
+def index():
+    print session
+    if 'username' in session:
+        return redirect(url_for('weather'))
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['POST'])
@@ -33,15 +25,16 @@ def login():
     session['username'] = request.form['username'].lower()
     return redirect(url_for('weather'))
 
+
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
+
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
-        print 'POSTING'
         user = FORECASTER.db.get_user(request.form['username'].lower())
         if user is not None:
             return 'User with that name already exists!'
@@ -52,20 +45,12 @@ def register():
     else:
         return render_template('register.html')
 
-@app.route('/')
-def index():
-    print session
-    if 'username' in session:
-        return redirect(url_for('weather'))
-    return render_template('index.html')
     
 @app.route('/weather', methods=['GET','POST'])
 def weather():
-
     USER = session['username']
     
     rf = request.form
-    
     if 'location' in rf:
         print rf['location'],type(rf['location'])
         FORECASTER.db.modify_user_location(USER, str(rf['location']))
@@ -75,6 +60,21 @@ def weather():
         
     forecast_data = FORECASTER.get_location_list(USER)
 
+    """
+    # on first login get current location
+    if len(forecast_data) == 0:
+        import requests
+        import urllib2
+        import json
+        url = 'http://freegeoip.net/json/8.8.8.8' #+request.remote_addr
+        print 'fetching from ',url
+        #req = urllib2.urlopen(url).read()
+        req = requests.get(url)
+        data = json.loads(req)
+        FORECASTER.db.modify_user_location(USER, data['zip_code'])
+        print data
+    """
+        
     return render_template('weather.html',
                            forecast_data = forecast_data,                           
                            icons=icons)
---
+++ icon_mapper.py
@@ -0,0 +1,16 @@
+
+icons = {
+    'clear-day': 'day-sunny',
+    'clear-night': 'night-clear',
+    'rain': 'rain',
+    'snow': 'snow',
+    'sleet': 'sleet',
+    'wind': 'strong-wind',
+    'fog': 'fog',
+    'cloudy': 'cloudy',
+    'partly-cloudy-day': 'day-cloudy',
+    'partly-cloudy-night': 'night-cloudy',
+    'hail': 'hail',
+    'thunderstorm': 'thunderstorm',
+    'tornado': 'tornado'
+}
